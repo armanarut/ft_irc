cdb4d750496f3c4251e82d1b68a0d9302369dddb
shakh7735 <91694982+shakh7735@users.noreply.github.com>
2023-02-17 15:18:58 +0400

    II bot

--- Bot/Bot.cpp
+++ Bot/Bot.cpp
@@ -4,9 +4,9 @@ Bot::Bot(char **av) :
                     m_host (av[1]),
                     m_port ((short)std::atoi(av[2])),
                     m_pass (av[3]),
-                    m_nick (av[4]) {}
+                    m_nick (av[4]) { readBook(); }
 
-Bot::~Bot(){}
+Bot::~Bot(){ createBook(); }
 
 void    Bot::start_bot() {
     struct sockaddr_in mysock;
@@ -17,7 +17,6 @@ void    Bot::start_bot() {
     mysock.sin_family = AF_INET; // Установить семейство адресов
     mysock.sin_port = htons(m_port); // Установить порт
     mysock.sin_addr.s_addr = inet_addr(m_host);; // Установить адрес
-    // std::cout  <<  mysock.sin_addr.s_addr << std::endl; // Cheking
     
         if (connect(m_fd, (struct sockaddr*)&mysock, sizeof (mysock)) != -1){
             std::cout << "Server connected!" << std::endl;
@@ -44,7 +43,8 @@ void    Bot::run(){
     {
         // std::cout << m_fd << std::endl;// cheking
         n++;
-        memset(buffer, 0, BUF_SIZE);
+        if(buffer[0])
+            memset(buffer, 0, BUF_SIZE);
         val = recv(m_fd, buffer, BUF_SIZE, 0);
         if (val == 0)
             break; 
@@ -55,79 +55,92 @@ void    Bot::run(){
             if (check_command(in_text))
                 break;  
         }
-        if (n > 50000000){
-            sending("PING " + m_nick + " " + m_host);
+        if (n > 500000000){
+            // sending("PING " + m_nick + " " + m_host);
             n = 0;
         }
-
-
     }
 }
 
-int Bot::check_command(std::string in_text){
-    std::string command = parsing_text(in_text);
+int Bot::check_command(std::string &in_text){
+    std::cout << "Incoming: " << in_text;
     std::string msg;
+    std::string command = parsing_text(in_text, &msg);
+    std::cout << "command-|" << command << "|   msg-|" << msg << "|" << std::endl;//cheking
 
-    if (m_user.empty())
+    if (m_user.empty() || command.empty())
         return 0;//std::cout << "User not identifit" << std::endl;
-    else if (command.empty())
+    else if (check_book(command, &msg))
         ;
-    else if (command == "TIME\r\n")
+    else if (command == "TIME")
         msg = get_time();
-    else if (command == "HELLO\r\n")
+    else if (command == "HELLO")
         msg = " Hello! I'm " + m_nick + "!";
-    else if (command == "HELP\r\n")
+    else if (command == "HELP")
         msg = help_command();
+    else if (command == "LEARN")
+        command_LEARN(&msg);
     else
         msg = " Anhaskanali hramana (xntrum em chisht grek)!";
    
     std::cout << ("PRIVMSG " + m_user + msg) << std::endl; // cheking
-    sending("PRIVMSG " + m_user + msg);
-    
+    if (!msg.empty())
+        sending("PRIVMSG " + m_user + msg);
     return 0;
 }
 
-std::string Bot::help_command(){
-    std::string str = " TIME - show time and date. HELLO - Hello! I'm bot!. HELP - command list.";
-    return str;
+bool    Bot::check_book(std::string &in_text, std::string *msg){
+    //Incoming: -> :ed!SH@localhost PRIVMSG gor :hello :sdf
+    std::map <std::string, std::string>::iterator it;
+
+    it = m_book.find(in_text);
+    if (it != m_book.end())
+    {
+        *msg = " " + (it->second);
+        return true;
+    }
+    return false;
 }
 
-std::string Bot::parsing_text(std::string in_text){
+void    Bot::command_LEARN(std::string *msg)
+{
+    std::string key = *msg;
+    std::string value = key.substr(key.find(":", 1) + 1, key.size());
+    key = key.substr(0, key.size() - value.size());
+    key = trim(key, " :");
+    value = trim(value," :");
+    toUpperCase(key);
+    *msg = " I'am learning :" + key;
+    if (key != "LEARN")
+        m_book[key] = value;
+    else
+        *msg = "Not learning :" + key;
+}
+
+std::string Bot::parsing_text(std::string &in_text, std::string *msg){
     m_user = "";
-    size_t pos = in_text.find("PRIVMSG");
-    if (!(pos < in_text.size()))
+    if (in_text.find("PRIVMSG") > in_text.size())
         return "";
     m_user = in_text.substr(1, in_text.find("!")-1);
-    std::string msg = in_text.substr(in_text.find(":", 1) + 1, in_text.size());
-    toUpperCase(msg);
-    std::cout << "user :" + m_user + " / command :" + msg; // cheking
-    return (msg);
+    std::string temp = in_text.substr(in_text.find(":", 1) + 1, in_text.size());
+    std::string command = temp;
+    temp = temp.substr(temp.find(":", 0) + 1, temp.size());
+    
+    if (temp.size() < command.size())
+        command = command.substr(0, command.size() - temp.size());
+    else
+        temp ="";
+    temp = trim(temp, " :\r\n\t");
+    command = trim(command," :\r\n");
+    toUpperCase(command);
+    *msg = temp;
+    return command;
 }
 
-std::string Bot::get_time(void)
-{
-    time_t rawtime;
-    struct tm * timeinfo;
-    time( &rawtime );
-    timeinfo = localtime ( &rawtime );
-    std::string dt = asctime (timeinfo);
-    std::vector<std::string>  data_time;
-    std::string temp;
-    int pos = 0;
-    
-    temp = dt.substr(pos, dt.find(" ", pos) - pos);
-    while(!temp.empty())
-    {
-        while(dt[pos] == ' ') pos++;
-        temp = dt.substr(pos, dt.find(" ", pos + 1) - pos);
-        pos += temp.size();
-        data_time.push_back(temp);
-    }
-    data_time[4].resize(4);
-    temp = " DATE : /" + data_time[0] + "/ " + data_time[2] + " ";
-    temp += data_time[1] + " " + data_time[4] + "\n";
-    temp += "TIME : " + data_time[3];
-    return temp;
+
+std::string Bot::help_command(){
+    std::string str = " < TIME > - show time and date. < HELLO > - Hello! I'm bot!. < HELP > - command list. < LEARN > - Learn < :command :reaple> ";
+    return str;
 }
 
 void	Bot::sending(const std::string& massage)
@@ -135,22 +148,37 @@ void	Bot::sending(const std::string& massage)
     std::string buffer = massage + "\r\n";
 
     if (send(m_fd, buffer.c_str(), buffer.length(), 0) < 0)
-        throw std::runtime_error("Error: can't send message to client.");
+        std::cout << "Error: can't send message to client." << std::endl;
 }
 
-std::string my_to_string(int num)
+void    Bot::readBook()
 {
-    std::stringstream stream;
-    std::string str;
-    stream << num;
-    stream >> str;
-    return str;
+    std::string _filename = "./book.txt";
+    std::string line;
+    std::ifstream in(_filename.c_str());
+    if (!in.is_open())
+        return;
+    std::string key;
+    std::string value;
+    while(std::getline(in, line)) {
+        key = line;
+        value = key.substr(key.find(":", 1) + 1, key.size());
+        key = key.substr(0, key.size() - value.size() - 1);
+        m_book[key] = value;
+    }
+    in.close();
 }
 
-void  toUpperCase(std::string& str)
+void    Bot::createBook()
 {
-    for(size_t i = 0 ; i < str.size(); ++i) 
-    { str[i] = std::toupper(str[i]); }
+    std::string _filename = "./book.txt";
+    std::ofstream out(_filename.c_str());
+    if (!out.is_open())
+        std::cerr << "failed to create file!" << std::endl;
+    for (std::map<std::string, std::string>::iterator it = m_book.begin(); it != m_book.end(); ++it) {
+        out<< it->first <<":" << it->second << std::endl;
+    }
+    out.close();
 }
 
 int main(int ac,char **av)
@@ -159,9 +187,9 @@ int main(int ac,char **av)
         std::cout << " /host/ /port/ /password/ /nick/" << std::endl;
         return 0;
     }
-    if (!is_all_digit(argv[2]))
+    if (!is_all_digit(av[2]))
         prog_error("Port must to by digital, and in range [1 to 65535]");
-    int port = std::atoi(argv[2]);
+    int port = std::atoi(av[2]);
     if ( port < 1 || port > 65535)
         prog_error("Port must to by digital, and in range [1 to 65535]");
     Bot bot(av);
--- Bot/Bot.hpp
+++ Bot/Bot.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <iostream>
-#include <sstream> 
+#include <sstream>
+#include <fstream>
 #include <vector>
+#include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -18,8 +20,15 @@
 
 #define BUF_SIZE 1024
 
+
+void        toUpperCase(std::string& str);
+void        prog_error(std::string err);
+bool        is_all_digit(std::string str);
 std::string my_to_string(int num);
-void  toUpperCase(std::string& str);
+std::string get_time(void);
+std::string ltrim(const std::string &s, const std::string &trimed);
+std::string rtrim(const std::string &s, const std::string &trimed);
+std::string trim(const std::string &s, const std::string &trimed);
 
 class Bot {
 
@@ -42,15 +51,19 @@ class Bot {
         std::string m_pass;
         std::string m_nick;
         std::string m_user;
+        std::map <std::string, std::string> m_book;
         
 
     private: // metods
-        void        run();;
+        void        run();
+        void        readBook();
+        void        createBook();
         void	    sending(const std::string& massage);
-        int         check_command(std::string in_text);;
-        std::string parsing_text(std::string in_text);;
-        std::string get_time(void);
+        int         check_command(std::string &in_text);
+        bool        check_book(std::string &in_text, std::string *msg);
+        std::string parsing_text(std::string &in_text, std::string *msg);
         std::string help_command();
+        void        command_LEARN(std::string *msg);
 };
 
 
